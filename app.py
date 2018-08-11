cb6f848f0a8993a1bde6b08d2497187b0ed960ec
bkzech <zech3632@gmail.com>
2018-08-11 11:17:21 -0500

    dev change to app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 x = input('Type number:')
 x = int(x)
-y = x + 3
-print('Your number is three less than',y)
-x = input('Type number:')
-x = int(x)
-y = x + 5
-print('Your number is not',y)
+y = x + 2
+print('Your number is two less than',y)
